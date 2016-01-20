ef2e1ce86319e556ca3e12df8244800ba2494917
Sergey <kuzmenkos1986@gmail.com>
2016-01-20 16:30:14 +0200

    content

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,36 +1,45 @@
 $(function(){
 
+    $('.swiper-container').each(function () {
+        Slider($(this));
+    });
+
+
 } );
-
-var Shablon = function (obj) {
-    this.obj = obj;
-
-
-    this.init();
-};
-Shablon.prototype = {
-    init: function () {
-        var self = this;
-
-        self.core = self.core();
-        self.core.build();
-    },
-    core: function () {
-        var self = this;
-
-        return {
-            addEvents: function () {
-
-            },
-            build: function () {
-                self.core.addEvents();
-            }
-        };
-    }
-};
 
 $(window).on({
     load: function () {
         
     }
 });
+
+var Slider = function (obj) {
+
+    //private properties
+    var _self = this,
+        _paginator = obj.find($('.slider__icon')),
+        _obj = obj;
+
+    //private methods
+    var _addEvents = function () {
+
+        },
+        _init = function () {
+            _addEvents();
+        };
+    if (_obj.hasClass('slider__wrap')) {
+        var _swiperPromo = new Swiper(_obj, {
+            slidesPerView: 1,
+            autoplay: 15000,
+            pagination: _paginator,
+            loop: true,
+            paginationClickable: true
+        });
+    }
+
+    //public properties
+
+    //public methods
+
+    _init();
+};
